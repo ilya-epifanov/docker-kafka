9b8b59472464febcefce2c8270693a2cc57fabd5
Ilya Epifanov <ilya.epifanov@datravel.com>
2015-04-20 12:22:56 +0300

    changing ownership only for data and logs directories

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ "$1" == 'kafka-server-start.sh' ]; then
 	mkdir -p -m a=rx,u+w /opt/kafka/{logs,data}
-	chown -R kafka /opt/kafka/*
+	chown -R kafka /opt/kafka/{logs,data}
 	set -- gosu kafka "$@"
 fi
 
